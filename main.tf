6053ffcb0a28906bd337a076c8ad470a85068dae
Ivan Zapata <ivanzapata2126@gmail.com>
2023-05-15 21:22:10 -0500

    push

--- main.tf
+++ main.tf
@@ -29,6 +29,8 @@ variable "imagebuild" {
 
 # Providing a reference to our default VPC
 resource "aws_default_vpc" "default_vpc" {
+  enable_dns_hostnames = true
+  enable_dns_support = true
 }
 
 # Providing a reference to our default subnets
@@ -54,8 +56,8 @@ resource "aws_ecs_task_definition" "my_first_task" {
       "essential": true,
       "portMappings": [
         {
-          "containerPort": 80,
-          "hostPort": 80,
+          "containerPort": 3000,
+          "hostPort": 3000,
           "protocol": "tcp"
         }
       ],
@@ -106,18 +108,18 @@ resource "aws_alb" "application_load_balancer" {
 # Creating a security group for the load balancer:
 resource "aws_security_group" "load_balancer_security_group" {
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
-  }
+        from_port = 80
+        to_port = 3000
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
 resource "aws_lb_target_group" "target_group" {
@@ -148,7 +150,7 @@ resource "aws_ecs_service" "my_first_service" {
   load_balancer {
     target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
     container_name   = "${aws_ecs_task_definition.my_first_task.family}"
-    container_port   = 80 # Specifying the container port
+    container_port   = 3000 # Specifying the container port
   }
 
   network_configuration {
@@ -161,19 +163,18 @@ resource "aws_ecs_service" "my_first_service" {
 
 resource "aws_security_group" "service_security_group" {
   ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    # Only allowing traffic in from the load balancer security group
-    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
-  }
+        from_port = 80
+        to_port = 3000
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
 #Show the load balancer url (app url)
